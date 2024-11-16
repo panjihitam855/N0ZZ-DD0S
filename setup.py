52ab1b6c275370a43ed22dcd48ff91c40969555e
KUNF24 <kunf5616@gmail.com>
2024-11-16 12:14:43 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+
+print("""[0] pip\n[1] pip3\nWhich one do you use?""")
+
+c = input(">>>: ")
+if c == "0":
+    os.system("apt-get install nodej-lts")
+    os.system("apt-get install npm")
+    os.system("npm install os")
+    os.system("npm install sys")
+    os.system("npm install threading")
+    os.system("npm install string")
+    os.system("git pull")
+elif c == "1":
+    os.system("apt-get install nodej-lts")
+    os.system("apt-get install npm")
+    os.system("npm install os")
+    os.system("npm install sys")
+    os.system("npm install threading")
+    os.system("npm install string")
+    os.system("git pull")
+    print("Done.")Enter
